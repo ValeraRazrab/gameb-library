7cd2cd095adf01b5291c1d2f0eb3829cc166a966
Valera <valerarazrab@gmail.com>
2019-02-16 21:13:03 +0300

    Init

---
+++ mywebviewgamb/src/main/java/com/gameapp/web/mywebviewgamb/MainActivity.java
@@ -0,0 +1,19 @@
+package com.gameapp.web.mywebviewgamb;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    MainActivity(Context c, String message){
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ mywebviewgamb/src/main/java/com/gameapp/web/mywebviewgamb/TestClass.java
@@ -0,0 +1,11 @@
+package com.gameapp.web.mywebviewgamb;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestClass {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+    }
+}
